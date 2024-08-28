4e999c7d16612c28070faed0e6353094f21cb166
Sesiverso <94802700+Sesiverso@users.noreply.github.com>
2024-08-28 11:13:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -38,6 +38,76 @@ const questions = [
             "Pau-brasil"
         ],
         answer: "Jequitibá"
+    },
+    {
+        question: "Em qual região a Mata Atlântica predomina?",
+        options: [
+            "Litoral",
+            "Interior",
+            "Centro-oeste",
+            "Noroeste"
+        ],
+        answer: "Litoral"
+    },
+    {
+        question: "Qual o principal clima da Mata Atlântica?",
+        options: [
+            "Tropical úmido",
+            "Temperado",
+            "Semiárido",
+            "Equatorial"
+        ],
+        answer: "Tropical úmido"
+    },
+    {
+        question: "Qual a árvore símbolo da Mata Atlântica?",
+        options: [
+            "Pau-Brasil",
+            "Jequitibá",
+            "Castanheira",
+            "Ipê"
+        ],
+        answer: "Pau-Brasil"
+    },
+    {
+        question: "Qual o animal símbolo da Mata Atlântica?",
+        options: [
+            "Mico-leão-dourado",
+            "Onça-pintada",
+            "Arara-azul",
+            "Tamanduá-bandeira"
+        ],
+        answer: "Mico-leão-dourado"
+    },
+    {
+        question: "O que contribuiu para a extinção da biodiversidade da Mata Atlântica?",
+        options: [
+            "Tráfico ilegal e desmatamento",
+            "Erosão",
+            "Invasão de espécies exóticas",
+            "Mudanças climáticas"
+        ],
+        answer: "Tráfico ilegal e desmatamento"
+    },
+    {
+        question: "Qual a área que a Mata Atlântica ocupa?",
+        options: [
+            "7% do território brasileiro",
+            "10% do território brasileiro",
+            "15% do território brasileiro",
+            "5% do território brasileiro"
+        ],
+        answer: "7% do território brasileiro"
+    },
+    {
+        question: "Quantas pessoas vivem na Mata Atlântica?",
+        options: [
+            "140 milhões",
+            "100 milhões",
+            "200 milhões",
+            "50 milhões"
+        ],
+        answer: "140 milhões"
     }
 ];
 
